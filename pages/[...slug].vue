37c0cafe8468079f70d70a3c686633a4bb0cbe1f
Paul Schnetzinger <p.schnetzinger@haelsi.at>
2024-11-13 19:38:22 +0100

    feat: try other rendering

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,7 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
+      <ContentRenderer :value="data" />
+    </ContentQuery>
   </main>
 </template>
